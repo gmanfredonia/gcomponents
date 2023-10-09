e3fb6d4e47143241ca1965265c4e4df36ee15e8d
Gianluca Manfredonia <gmanfredonia@inwind.it>
2023-10-09 11:56:47 +0200

    pulizia del codice del componente gtable

---
+++ projects/gcomponents/src/lib/components/gtable-page-size/gtable-page-size.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IDropdownItem } from '../../models/idropdown-item.model';
+
+@Component({
+  selector: 'gtable-page-size',
+  templateUrl: './gtable-page-size.component.html',
+  styleUrls: ['./gtable-page-size.component.scss'],
+})
+export class GTablePageSizeComponent {
+  onModelChange(event: IDropdownItem) {
+    this.changePageSize.emit(parseInt(event.id));
+  }
+
+  @Input() labelBefore?: string;
+  @Input() labelAfter?: string;
+  @Output() changePageSize = new EventEmitter<number>();
+
+  formGroup: FormGroup;
+
+  items: IDropdownItem[];
+  constructor(private fb: FormBuilder) {
+    this.items = [
+      { id: '10', name: '10' },
+      { id: '50', name: '50' },
+      { id: '100', name: '100' },
+    ];
+
+    this.formGroup = fb.group({
+      pageSize: ['10'],
+    });
+  }
+}
